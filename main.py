f2f7db12ed722b9242baa00829684f93ad416472
Matheus-Barkokebas <matheusbarkcaribe@gmail.com>
2024-10-22 13:30:22 -0300

    Inicio

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("aaaa")
+from art import text2art
+
+welcome_art = text2art("UrbanAlert", font='standard')
+logo_art = text2art("Monitore  e  Previna", font='standard')
+
+print(welcome_art)
+print(logo_art)
