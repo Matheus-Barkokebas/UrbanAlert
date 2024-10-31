204a465ca36b40cea8a50e545c5d15bcb34f15bb
Lucascs88 <166466575+Lucascs88@users.noreply.github.com>
2024-10-31 16:28:54 -0300

    listar ocorrencia

--- ocorrencia.py
+++ ocorrencia.py
@@ -31,6 +31,30 @@ def criar_ocorrencia (qualOcorrencia, estadoOcorrencia, cidadeOcorrencia, bairro
     dados_ocorrencia.append(nova_ocorrencia)
     salvar_dados (dados_ocorrencia)
     print("ocorrencia salva com sucesso.")
+
+def consultar_ocorrencia ():
+    dados_ocorrencia = carregar_dados()
+
+    if dados_ocorrencia:
+        for ocorrencia in dados_ocorrencia:
+            print(f"nome da ocorrencia {ocorrencia['qualOcorrencia']}")
+            print(f"estado da ocorrencia {ocorrencia['estadoOcorrencia']}")
+            print(f"cidade da ocorrencia {ocorrencia['cidadeOcorrencia']}")
+            print(f"bairro da ocorrencia {ocorrencia['bairroOcorrencia']}")
+            print(f"nome da rua {ocorrencia['ruaOcorrencia']}")
+            print(f"hora da ocorrencia {ocorrencia['horaOcorrencia']}") 
+            print(f"detalhe da ocorrencia {ocorrencia['detalheOcorrencia']}")
+    else:
+        print("Esses dados nao existem")
+
+
+
+
+
+
+
+
+
     
 
     
